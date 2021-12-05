9e2fef2f54ea26aca11037b0bf996176ff8e356a
Jonas Röger <jonas.roeger@tu-dortmund.de>
2021-12-05 01:32:38 +0100

    add day03-2 try

--- src/AoC2021/Day03.hs
+++ src/AoC2021/Day03.hs
@@ -1,15 +1,30 @@
 module AoC2021.Day03 where
 
-import           Data.Char
-import           Data.Functor ((<&>))
-import           Solver       (Solver)
-import qualified Text.Parsec  as P
-
+import           Control.Monad             (forM_, unless, when)
+import qualified Control.Monad.Trans.State as S
+import           Data.Array                ((!))
+import qualified Data.Array                as A
+import           Data.Char                 (digitToInt)
+import           Data.Functor              ((<&>))
+import           Data.List                 (partition)
+import           Solver                    (Solver)
+import qualified Text.Parsec               as P
 
 -- Running count of densities for each column of bits
 type Densities = [Int]
-
 type Parser = P.Parsec String ()
+
+-- State to keep during LifeSupport calculation
+data LSCalcState = LSCalcState
+  { oxygenDensities   :: Densities
+  , co2Densities      :: Densities
+  , oxygenBitStrings  :: [A.Array Int Char]
+  , co2BitStrings     :: [A.Array Int Char]
+  , lsCalcBitIndex    :: Int
+  , lsCalcBitIndexMax :: Int }
+
+-- LifeSupport calculation action
+type LSCalcAction = S.State LSCalcState
 
 weight :: Char -> Int
 weight '0' = -1
@@ -54,5 +69,73 @@ solve01 input = case P.runParser parseAllBitStrings () "" input <&> calculateCol
                          epsilonStr = map flipBit gammaStr
                      in show $ bin2Int gammaStr * bin2Int epsilonStr
 
+indexBitString :: [Char] -> A.Array Int Char
+indexBitString bs = A.listArray (1,n) bs
+  where n = length bs
+
+-- Modify a density count, as if the given bit string were never part of it
+removeFromDensities :: Densities -> String -> Densities
+removeFromDensities ds = applyDensities ds . map flipBit
+
+removeAllFromDensities :: Densities -> [String] -> Densities
+removeAllFromDensities = foldl removeFromDensities
+
+-- Remove BitStrings from oxygenBitStrings according to the dominant bit rule
+-- and adjust densities of the list accordingly
+filterOxygenListAction :: LSCalcAction ()
+filterOxygenListAction = do
+  i <- S.gets lsCalcBitIndex
+  d <- S.gets oxygenDensities
+  bss <- S.gets oxygenBitStrings
+  let requiredBit = if 0 <= (d!!i-1)
+                      then '1' 
+                      else '0'
+  let (bss', rm) = partition (\a -> (a!i) == requiredBit) bss
+  S.modify $ \s -> s { oxygenDensities = removeAllFromDensities d (map A.elems rm)
+                     , oxygenBitStrings = bss' }
+
+-- Remove BitStrings from co2BitStrings according to the dominant bit rule
+-- and adjust densities of the list accordingly
+filterCo2ListAction :: LSCalcAction ()
+filterCo2ListAction = do
+  i <- S.gets lsCalcBitIndex
+  d <- S.gets co2Densities
+  bss <- S.gets co2BitStrings
+  let requiredBit = if 0 >= (d!!i-1)
+                      then '0' 
+                      else '1'
+  let (bss', rm) = partition (\a -> (a!i) == requiredBit) bss
+  S.modify $ \s -> s { co2Densities = removeAllFromDensities d (map A.elems rm)
+                     , co2BitStrings = bss' }
+
+calculateLifeSupportAction :: LSCalcAction ()
+calculateLifeSupportAction = do
+  oxygenDone <- S.gets $ (<=1) . length . oxygenBitStrings
+  co2Done <- S.gets $ (<=1) . length . co2BitStrings
+  unless oxygenDone filterOxygenListAction
+  unless co2Done filterCo2ListAction
+  unless (co2Done && oxygenDone) $ do
+    i <- S.gets lsCalcBitIndex
+    n <- S.gets lsCalcBitIndexMax
+    S.modify $ \s -> s { lsCalcBitIndex = i+1 }
+    unless (i==n) calculateLifeSupportAction
+
+
+calculateLifeSupportBitStrings :: [[Char]] -> ([Char], [Char])
+calculateLifeSupportBitStrings bss = (oxygenBs, co2Bs)
+  where
+    d = calculateColumnWiseDensities bss
+    n = length d
+    bss' = map indexBitString bss
+    initialState = LSCalcState d d bss' bss' 1 n
+    endState = S.execState calculateLifeSupportAction initialState
+    oxygenBs = A.elems . head . oxygenBitStrings $ endState
+    co2Bs = A.elems . head . co2BitStrings $ endState
+
+
+
 solve02 :: Solver
-solve02 = undefined
+solve02 input = case P.runParser parseAllBitStrings () "" input of
+  Left e -> show e
+  Right bss -> let (oxyStr, co2Str) = calculateLifeSupportBitStrings bss
+               in show $ bin2Int oxyStr * bin2Int co2Str
--- test/AoC2021/Day03Spec.hs
+++ test/AoC2021/Day03Spec.hs
@@ -2,7 +2,8 @@ module AoC2021.Day03Spec (spec) where
 
 import           Test.Hspec    (Spec, describe, it, shouldBe)
 
-import           AoC2021.Day03 (solve01)
+import           AoC2021.Day03
+import qualified Text.Parsec   as P
 
 input01 :: String
 input01 = "00100\n\
@@ -18,11 +19,29 @@ input01 = "00100\n\
           \00010\n\
           \01010\n"
 
+input02 :: String
+input02 = input01
+
 result01 :: String
 result01 = "198"
+
+result02 :: String
+result02 = "230"
+
+bitStrings02 :: [[Char]]
+bitStrings02 = case P.runParser parseAllBitStrings () "" input02 of
+  Right bss -> bss
+
 
 spec :: Spec
 spec = do
   describe "Day03-01" $ do
     it "solves example input" $ do
       solve01 input01 `shouldBe` result01
+  describe "Day03-02" $ do
+    it "has correct life support bit strings" $ do
+      let (oxy,co2) = calculateLifeSupportBitStrings bitStrings02
+      oxy `shouldBe` "10111"
+      co2 `shouldBe` "01010"
+    it "solves example input" $ do
+      solve02 input02 `shouldBe` result02
