9a09783ce1fa588e958bd1463be242c406f5e150
Jonas Röger <jonas.roeger@tu-dortmund.de>
2021-12-07 23:44:05 +0100

    fix parsers

--- src/Parsers.hs
+++ src/Parsers.hs
@@ -15,7 +15,7 @@ parseSpace1 :: Parser String
 parseSpace1 = P.many1 (P.char ' ')
 
 parseLinesWith :: Parser a -> Parser [a]
-parseLinesWith p = P.sepBy p P.endOfLine
+parseLinesWith p = P.sepEndBy p (P.many1 P.endOfLine)
 
 
 surroundBy :: Parser b -> Parser a -> Parser b
