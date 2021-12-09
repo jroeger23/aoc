68ec248ca10568c9864d58756c72c10a0df7729b
Jonas Röger <jonas.roeger@tu-dortmund.de>
2021-12-09 19:10:14 +0100

    add efficient Day06 solver

--- src/AoC2021/Day06.hs
+++ src/AoC2021/Day06.hs
@@ -1,8 +1,40 @@
 module AoC2021.Day06 where
 
-import           Parsers
-import qualified Text.Parsec as P
+import           Data.Semigroup        (stimes)
+import           Numeric.LinearAlgebra ((#>), (><))
+import qualified Numeric.LinearAlgebra as M
+import           Parsers               (Parser, parseIntegral)
+import qualified Text.Parsec           as P
 
+-- Linear (in growth days) time algorithm ----------------------------------------------------------
+-- See Leslie Matrices (https://en.wikipedia.org/wiki/Leslie_matrix)
+
+leslieMatrix :: M.Matrix Double -- Use double, to use optimized matrix multiplication of hmatrix
+leslieMatrix = (9><9) [ 0, 1, 0, 0, 0, 0, 0, 0, 0 -- Count 1 -> 0
+                      , 0, 0, 1, 0, 0, 0, 0, 0, 0 -- Count 2 -> 1
+                      , 0, 0, 0, 1, 0, 0, 0, 0, 0 -- Count 3 -> 2
+                      , 0, 0, 0, 0, 1, 0, 0, 0, 0 -- Count 4 -> 3
+                      , 0, 0, 0, 0, 0, 1, 0, 0, 0 -- Count 5 -> 4
+                      , 0, 0, 0, 0, 0, 0, 1, 0, 0 -- Count 6 -> 5,
+                      , 1, 0, 0, 0, 0, 0, 0, 1, 0 -- Count 7 -> 6, 0 -> 6 (re-spawn, instead of dying)
+                      , 0, 0, 0, 0, 0, 0, 0, 0, 1 -- Count 8 -> 7
+                      , 1, 0, 0, 0, 0, 0, 0, 0, 0 -- Count 9 -> 8, 0 -> 8 (spawn child with 8)
+                      ]
+
+projectNDays :: Int -> M.Matrix Double -> M.Matrix Double
+projectNDays = stimes
+
+day0 :: [Int] -> M.Vector Double
+day0 = M.vector . zipWith count [0..8] . repeat
+  where count x = fromIntegral . length . filter (==x)
+
+solve' :: Int -> [Int] -> Int
+solve' days fishes = round $ M.sumElements dayN
+  where dayN = projectNDays days leslieMatrix #> day0 fishes
+----------------------------------------------------------------------------------------------------
+
+
+-- Exponential (in growth days) time algorithm -----------------------------------------------------
 -- | Calculate the number of fishes spawned by one fish within d days
 f :: Int -> Int
 f d = (1+) . sum . map (f . childOffset) $ [0 .. nSpawn - 1]
@@ -14,6 +46,7 @@ f d = (1+) . sum . map (f . childOffset) $ [0 .. nSpawn - 1]
 -- | Total number of fishes within d days, given a list of fishes with their initial counter
 solve :: Int -> [Int] -> Int
 solve d = sum . map (f . (d-))
+----------------------------------------------------------------------------------------------------
 
 parseIntegerList :: Parser [Int]
 parseIntegerList = P.sepBy parseIntegral (P.char ',')
@@ -21,11 +54,11 @@ parseIntegerList = P.sepBy parseIntegral (P.char ',')
 solve01 :: String -> String
 solve01 input = case P.runParser parseIntegerList () "" input of
   Left e   -> show e
-  Right is -> show (solve 80 is)
+  Right is -> show (solve' 80 is)
 
 
 -- | Way to inefficient, maybe solve the sum of f?
 solve02 :: String -> String
 solve02 input = case P.runParser parseIntegerList () "" input of
   Left e   -> show e
-  Right is -> show (solve 256 is)
+  Right is -> show (solve' 256 is)
