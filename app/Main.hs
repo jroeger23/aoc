c99530ef9874611a2dfa3a4a28d80a8ff81df969
Jonas Röger <jonas.roeger@tu-dortmund.de>
2021-12-09 20:50:03 +0100

    change Framework to infer input

--- app/Main.hs
+++ app/Main.hs
@@ -15,6 +15,7 @@ import qualified AoC2021.Day03      as D03
 import qualified AoC2021.Day04      as D04
 import qualified AoC2021.Day05      as D05
 import qualified AoC2021.Day06      as D06
+import           Data.Maybe         (fromMaybe)
 
 
 implementedSolvers :: [(String, Solver)]
@@ -31,11 +32,12 @@ implementedSolvers = [ ("day01-1", D01.solve01)
                      ]
 
 printUsage :: IO ()
-printUsage = putStrLn "Usage: ./aoc2021 <solver> <inputfile>"
+printUsage = putStrLn "Usage: ./aoc2021 <solver> [inputfile]"
 
-firstTwoArgs :: IO (String, String)
-firstTwoArgs = getArgs >>= \case
-  (a:b:_) -> return (a,b)
+aocArgs :: IO (String, Maybe String)
+aocArgs = getArgs >>= \case
+  (a:b:_) -> return (a,Just b)
+  (a:_)   -> putStrLn "No input given. Assuming location..." >> return (a, Nothing )
   _       -> printUsage >> exitFailure
 
 runSolver :: Solver -> String -> IO ()
@@ -50,9 +52,16 @@ runSolver solver file = do
 printSolvers :: IO ()
 printSolvers = forM_ (map fst implementedSolvers) (putStrLn.(" - "<>))
 
+inferFileFromSolver :: String -> String
+inferFileFromSolver s = let d = take 5 s
+                        in if length s >= 5
+                            then "inputs/" ++ d
+                            else ""
+
 main :: IO ()
 main = do
-  (nSolver, file) <- firstTwoArgs
+  (nSolver, mFile) <- aocArgs
+  let file = fromMaybe (inferFileFromSolver nSolver) mFile
   case lookup nSolver implementedSolvers of
     Just solver -> do
       putStrLn $ "Running solver " ++ nSolver ++ " on " ++ file
